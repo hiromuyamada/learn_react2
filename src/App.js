9322ea6c07a6a6b7ad009c46a988a141f35cb939
Hiromu Yamada <hiromu@HiromunoMacBook-Air.local>
2022-06-14 02:14:02 +0900

    section4 practice csslib

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { InlineStyle } from './components/InlineStyle';
+import { Emotion } from './components/Emotion';
+import { CssModules } from './components/CssModules';
+import { StyledJsx } from './components/StyledJsx';
+import { StyledComponents } from './components/StyledComponents';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <InlineStyle />
+    <CssModules />
+    <StyledJsx />
+    <StyledComponents />
+    <Emotion />
     </div>
   );
 }
