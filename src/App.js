d6cec8cee0b94095a78f1ed26e60bf8feb285e4e
Hiromu Yamada <hiromu@HiromunoMacBook-Air.local>
2022-06-22 02:05:35 +0900

    create atoms molecules organisms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { PrimaryButton } from './components/atoms/button/PrimaryButton';
+import { SecondaryButton } from './components/atoms/button/SecondaryButton';
+import { SearchInput } from './components/molecules/SearchInput';
+import { UserCard } from './components/organisms/user/UserCard';
+
+const user ={
+  image:"https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
+  name:"hiromu",
+  email:'example@co',
+  phone:"090-000-0000",
+  company:{
+    name:"test株式会社"
+  },
+  website:"www.com",
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>テスト</SecondaryButton>
+      <br />
+      <SearchInput></SearchInput>
+      <UserCard user={user}/>
     </div>
   );
 }
---
+++ src/components/molecules/SearchInput.jsx
@@ -0,0 +1,26 @@
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { Input } from "../atoms/input/Input";
+import styled from "styled-components";
+
+export const SearchInput = () =>{
+    return (
+        <SContainer>
+        <div>
+            <Input placeholder="検索条件を入力" />
+            <SButtonWrapper>
+                <PrimaryButton>検索</PrimaryButton>
+            </SButtonWrapper>
+        </div>
+</SContainer>
+    );
+};
+
+const SButtonWrapper = styled.div`
+padding-left 8px;
+
+`
+const SContainer = styled.div`
+display:flex;
+aline-items:center;
+
+`
