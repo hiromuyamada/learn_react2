33cff19dff226bc03a5a5d4b5b17f0b71144cbee
Hiromu Yamada <hiromu@HiromunoMacBook-Air.local>
2022-06-22 03:04:33 +0900

    create pages

--- src/App.js
+++ src/App.js
@@ -4,27 +4,24 @@ import { PrimaryButton } from './components/atoms/button/PrimaryButton';
 import { SecondaryButton } from './components/atoms/button/SecondaryButton';
 import { SearchInput } from './components/molecules/SearchInput';
 import { UserCard } from './components/organisms/user/UserCard';
+import { HeaderOnly } from './components/templates/HeaderOnly';
+// import { BrowserRouter, Router } from 'react-router-dom';
+import { DefaultLayout } from './components/templates/DefaultLayout';
+import { Router } from './router/router';
 
-const user ={
-  image:"https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
-  name:"hiromu",
-  email:'example@co',
-  phone:"090-000-0000",
-  company:{
-    name:"test株式会社"
-  },
-  website:"www.com",
-}
 
 function App() {
   return (
-    <div className="App">
-      <PrimaryButton>テスト</PrimaryButton>
-      <SecondaryButton>テスト</SecondaryButton>
-      <br />
-      <SearchInput></SearchInput>
-      <UserCard user={user}/>
-    </div>
+    // <BrowserRouter>
+    // <DefaultLayout>
+    //   <PrimaryButton>テスト</PrimaryButton>
+    //   <SecondaryButton>テスト</SecondaryButton>
+    //   <br />
+    //   <SearchInput></SearchInput>
+    //   <UserCard user={user}/>
+    // </DefaultLayout>
+    // </BrowserRouter>
+    <Router />
   );
 }
 
---
+++ src/components/atoms/layout/Footer.jsx
@@ -0,0 +1,20 @@
+import {Link} from "react-router-dom";
+import styled from "styled-components";
+
+export const Footer = () =>{
+    return(
+        <SFooter>
+            &copy;2022 text Inc.
+        </SFooter>
+    );
+};
+
+const SFooter = styled.div`
+    background-color:#11999e;
+    color:#fff;
+    text-align:center;
+    padding:8px 0;
+    postion:fixed;
+    bottom:0;
+    width:100%;
+`;
